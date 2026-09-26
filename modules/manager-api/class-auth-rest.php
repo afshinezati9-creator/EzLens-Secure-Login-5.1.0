14f0f5a4b8d3081102e5b25a5a4bca4fec7d4c0c
Afshin Ezati <afshinezati9@gmail.com>
2026-09-26 09:27:12 +0330

    fix(cors): allow private network preflight from Flutter web

--- modules/manager-api/class-auth-rest.php
+++ modules/manager-api/class-auth-rest.php
@@ -48,6 +48,7 @@ class EzLens_Manager_Auth_REST {
 		header( 'Access-Control-Allow-Credentials: true' );
 		header( 'Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS' );
 		header( 'Access-Control-Allow-Headers: Authorization, X-EzLens-Token, Content-Type, X-WP-Nonce, X-Requested-With, Accept, Origin' );
+		header( 'Access-Control-Allow-Private-Network: true' );
 		header( 'Access-Control-Max-Age: 600' );
 		header( 'Access-Control-Expose-Headers: X-WP-Total, X-WP-TotalPages, Link' );
 		header( 'Vary: Origin' );
